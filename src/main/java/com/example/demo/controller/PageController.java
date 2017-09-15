a499aa2ac08087f7ada921dd371eaa063cbbf4ac
Jessica Susanto <jessciacia1097@gmail.com>
2017-09-15 17:31:57 +0700

    Tutor 2

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController
+{
+	@RequestMapping("/hello")
+	public String index ()
+	{
+		return "hello";
+	}
+	
+	@RequestMapping("/greeting")
+	public String greeting (@RequestParam(value = "name", required = false, defaultValue = "dunia") String
+			name, Model model)
+	{
+	model.addAttribute ("name", name);
+	return "greeting";
+	}
+	
+	@RequestMapping(value = {"/greeting", "greeting/{name}"})
+	public String greetingPath (@PathVariable Optional<String> name, Model model)
+	{
+		if(name.isPresent()) {
+			model.addAttribute("name", name.get());
+		}else {
+			model.addAttribute("name", "dengklek");
+		}
+		return "greeting";
+	}
+	
+	@RequestMapping("/perkalian")	
+	public String perkalian (@RequestParam(value = "a", required = false, defaultValue = "0" ) String
+			varA, @RequestParam(value = "b", required = false, defaultValue = "0" ) String
+			varB, Model model)
+	{
+		System.out.println(varA + " x " + varB);
+	model.addAttribute ("varA", varA);
+	model.addAttribute ("varB", varB);
+	model.addAttribute ("result", Integer.valueOf(varA) * Integer.valueOf(varB));
+	return "perkalian";
+	}
+
+
+}
